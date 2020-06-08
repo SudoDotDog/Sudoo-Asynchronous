6b4c7d8b2c42a5aec5ce9d2813221d1f26175d58
WMXPY <wm@mengw.io>
2020-06-08 11:15:34 -0500

    implement replacement and ignore for ordered

--- src/runner/unordered-list.ts
+++ src/runner/unordered-list.ts
@@ -1,22 +1,22 @@
 /**
  * @author WMXPY
  * @namespace Asynchronous_Runner
- * @description Ordered List 
+ * @description Unordered List 
  */
 
 import { AsyncExecutableArray, KeyedResult, PromiseFunction } from "../declare";
 
-export type OrderedListRunnerConditionedResult<T> = {
+export type NestedOrderedListRunnerConditionedResult<T> = {
 
     readonly succeed: Record<number, T>;
     readonly failed: Record<number, any>;
 };
 
-export class OrderedListRunner<T extends any = any> {
+export class NestedOrderedListRunner<T extends any = any> {
 
     public static create<T extends any = any>(functions: AsyncExecutableArray<T>) {
 
-        return new OrderedListRunner<T>(functions);
+        return new NestedOrderedListRunner<T>(functions);
     }
 
     private readonly _functions: AsyncExecutableArray<T>;
@@ -26,33 +26,7 @@ export class OrderedListRunner<T extends any = any> {
         this._functions = functions;
     }
 
-    public async startReplaceFailed(whenFailed: any, ...args: any[]): Promise<Record<number, T>> {
-
-        const evaluated: OrderedListRunnerConditionedResult<T> = await this.start(...args);
-        const succeedKeys: string[] = Object.keys(evaluated.succeed);
-        const failedKeys: string[] = Object.keys(evaluated.failed);
-
-        const result: Record<number, T> = {} as Record<number, T>;
-
-        for (const succeedKey of succeedKeys) {
-            const numberedKey: number = Number(succeedKey);
-            result[numberedKey] = evaluated.succeed[numberedKey];
-        }
-        for (const failedKey of failedKeys) {
-            const numberedKey: number = Number(failedKey);
-            result[numberedKey] = whenFailed;
-        }
-
-        return result;
-    }
-
-    public async startIgnoreFiled(...args: any[]): Promise<Record<number, T>> {
-
-        const evaluated: OrderedListRunnerConditionedResult<T> = await this.start(...args);
-        return evaluated.succeed;
-    }
-
-    public async start(...args: any[]): Promise<OrderedListRunnerConditionedResult<T>> {
+    public async start(...args: any[]): Promise<NestedOrderedListRunnerConditionedResult<T>> {
 
         const awaitables: Array<Promise<KeyedResult<T>>> =
             this._functions.map((each: PromiseFunction<T>, index: number) => {
@@ -74,8 +48,8 @@ export class OrderedListRunner<T extends any = any> {
             });
 
         const executed: Array<KeyedResult<T>> = await Promise.all(awaitables);
-        const results: OrderedListRunnerConditionedResult<T> = executed.reduce(
-            (previous: OrderedListRunnerConditionedResult<T>, current: KeyedResult<T>) => {
+        const results: NestedOrderedListRunnerConditionedResult<T> = executed.reduce(
+            (previous: NestedOrderedListRunnerConditionedResult<T>, current: KeyedResult<T>) => {
 
                 if (current.succeed === true) {
                     return {
@@ -101,7 +75,7 @@ export class OrderedListRunner<T extends any = any> {
             }, {
                 succeed: [],
                 failed: {},
-            } as OrderedListRunnerConditionedResult<T>,
+            } as NestedOrderedListRunnerConditionedResult<T>,
         );
 
         return results;
