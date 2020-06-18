313c1f5f87ba60d4c271e91bcd721cc80162c3d7
WMXPY <wm@mengw.io>
2020-06-18 09:51:31 -0500

    implement sleep unit test, improve sleep function

--- src/util/sleep.ts
+++ src/util/sleep.ts
@@ -12,29 +12,61 @@ export class Sleep {
     }
 
     private _timer: any;
-    private _resolve: (time: number) => void | null;
+    private _startTime: number | null;
+    private _resolve: ((time: number) => void) | null;
 
     private constructor() {
 
         this._timer = null;
+        this._startTime = null;
         this._resolve = null;
     }
 
     public start(time: number): Promise<number> {
 
         const startTime: number = Date.now();
+        this._startTime = startTime;
 
         return new Promise<number>((resolve: (time: number) => void) => {
 
             this._resolve = resolve;
             this._timer = setTimeout(() => {
 
-                const endTime: number = Date.now();
+                this._resetValues();
 
-                this._resolve = null;
-                this._timer = null;
-                resolve(endTime - startTime);
+                const endTime: number = Date.now();
+                const difference: number = endTime - startTime;
+
+                resolve(difference);
             }, time);
         });
+    }
+
+    public stop(): number {
+
+        if (this._timer === null
+            || this._startTime === null
+            || this._resolve === null) {
+
+            return NaN;
+        }
+
+        clearTimeout(this._timer);
+
+        const endTime: number = Date.now();
+        const difference: number = endTime - this._startTime;
+
+        this._resolve(difference);
+        this._resetValues();
+
+        return difference;
+    }
+
+    private _resetValues(): void {
+
+        this._timer = null;
+        this._startTime = null;
+        this._resolve = null;
+        return;
     }
 }
--- test/unit/util/sleep.test.ts
+++ test/unit/util/sleep.test.ts
@@ -20,4 +20,23 @@ describe('Given {Sleep} Class', (): void => {
 
         expect(sleep).to.be.instanceOf(Sleep);
     });
+
+    it('should be able to sleep in a short amount of time', async (): Promise<void> => {
+
+        const sleep: Sleep = Sleep.create();
+        const value: number = 20;
+
+        const startTime: number = Date.now();
+
+        const result: number = await sleep.start(value);
+
+        const endTime: number = Date.now();
+        const difference: number = endTime - startTime;
+
+        expect(result).to.be.below(value + 5);
+        expect(result).to.be.above(value - 5);
+
+        expect(difference).to.be.below(value + 5);
+        expect(difference).to.be.above(value - 5);
+    });
 });
