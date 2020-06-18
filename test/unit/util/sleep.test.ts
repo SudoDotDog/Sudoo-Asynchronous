fa007a7ad12d200b9493e93cc0009b706a51ed30
WMXPY <wm@mengw.io>
2020-06-18 09:53:29 -0500

    implement sleep unit tests

--- test/unit/util/sleep.test.ts
+++ test/unit/util/sleep.test.ts
@@ -39,4 +39,32 @@ describe('Given {Sleep} Class', (): void => {
         expect(difference).to.be.below(value + 5);
         expect(difference).to.be.above(value - 5);
     });
+
+    it('should be able to sleep and stop break', async (): Promise<void> => {
+
+        const sleep: Sleep = Sleep.create();
+        const initialValue: number = 50;
+        const actualValue: number = 20;
+
+        const startTime: number = Date.now();
+
+        let responseDifference: number = NaN;
+
+        setTimeout(() => {
+            responseDifference = sleep.stop();
+        }, actualValue);
+        const result: number = await sleep.start(initialValue);
+
+        const endTime: number = Date.now();
+        const difference: number = endTime - startTime;
+
+        expect(responseDifference).to.be.below(actualValue + 5);
+        expect(responseDifference).to.be.above(actualValue - 5);
+
+        expect(result).to.be.below(actualValue + 5);
+        expect(result).to.be.above(actualValue - 5);
+
+        expect(difference).to.be.below(actualValue + 5);
+        expect(difference).to.be.above(actualValue - 5);
+    });
 });
